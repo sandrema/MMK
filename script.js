2d2a5cb4c0ced5f5ed2382da327710bd3b00e9cf
sandrema <promokaska101@gmail.com>
2022-02-08 19:35:34 +0300

    Немножечко интеравтива)

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let checkBox = document.querySelectorAll('.check-box');
+
+
+
+checkBox.forEach((el,i)=>{
+    checkBox[i].addEventListener('click', (e)=>{
+        if(el.classList.contains('active')){
+            el.classList.remove('active');
+            el.src='imges/check-all-false.png';
+        } else{
+            el.classList.add('active');
+            el.src='imges/check-all-true.png';
+            console.log(el);
+        }
+    });
+});
